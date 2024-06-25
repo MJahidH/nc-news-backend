cfb2a7a349e43049ca0cbf29549c389ebc45f4bf
MJahidH <m.jahid.h@hotmail.com>
2024-06-25 12:51:52 +0100

    The pool now xonnects to the virtual machine that has been setup.All these values should not be on github and stored as env variables.

--- db/connection.js
+++ db/connection.js
@@ -2,13 +2,23 @@ const { Pool } = require("pg");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const config = {
+const localConfig = {
   user: process.env.USER ,
   host:process.env.HOST ,
   database:process.env.DATABASE ,
   port:process.env.PORT,
 };
 
-const pool = new Pool(config);
+const remoteConfig = {
+  user: process.env.PGSQLUSER ,
+  host:process.env.PGSQLHOST ,
+  database:process.env.PGSQLDATABASE ,
+  port:process.env.PGSQLPORT,
+  password:process.env.PGSQLPASSWORD 
+};
+
+
+
+const pool = new Pool(remoteConfig);
 
 module.exports = pool;
